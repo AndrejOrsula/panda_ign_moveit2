1bb39c9cbf4169dd7aae7072e5ca4204e9f02f90
Andrej Orsula <orsula.andrej@gmail.com>
2021-12-12 15:00:13 +0100

    Add pre-commit configuration and setup
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

---
+++ .git_hooks/pre-commit_convert_xacros.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Pre-commit hook that convert xacros to URDF/SDF/SRDF. It makes sure that edits in xacros propagate to these files.
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+REPO_DIR="$(dirname "${SCRIPT_DIR}")"
+
+# Convert xacros to URDF, SDF and SRDF
+${REPO_DIR}/panda_description/scripts/xacro2urdf.bash
+${REPO_DIR}/panda_description/scripts/xacro2sdf.bash
+${REPO_DIR}/panda_moveit_config/scripts/xacro2srdf.bash
